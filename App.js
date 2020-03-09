04c6ec10e675b21dfc9e550ccb0938652ee3dd24
Shamshad Khan <shamshad.khan@nagarro.com>
2020-03-09 17:33:09 +0530

    props updated

--- App.js
+++ App.js
@@ -1,32 +1,30 @@
-import React from 'react';
-import {View} from 'react-native';
+import React from "react";
+import { View } from "react-native";
 
-import CircularCarousel from './CircularCarousel';
+import CircularCarousel from "./CircularCarousel";
 
 export default class App extends React.Component {
-  render() {
+	render() {
+		const dataSource = [
+			{ url: "", color: "#FE0404" },
+			{ url: "", color: "#522A73" },
+			{ url: "", color: "#008200" },
+			{ url: "", color: "#034223" },
+			{ url: "", color: "#015280" }
+		];
 
-    const dataSource = [
-        { url: "", color: "#FE0404" },
-        { url: "", color: '#522A73' },
-        { url: "", color: "#008200" },
-        { url: "", color: "#034223" },
-        { url: "", color: "#015280" },
-      ];
-
-    return (
-      <View style={{ flex:1, justifyContent: 'center', alignItems:'center' }}>
-      <CircularCarousel 
-        dataSource={dataSource}
-        onItemPress= {(item) => { 
-                    console.log(item) ;
-                    }
-        }
-        style={{ height: 200, width: 350}} 
-        itemStyle={{ width: 110, height:120 }}
-        radius={100}
-      />
-      </View>
-    );
-  }
+		return (
+			<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+				<CircularCarousel
+					dataSource={dataSource}
+					onItemPress={(item) => {
+						console.log(item);
+					}}
+					containerDim={{ height: 200, width: 350 }}
+					itemDim={{ width: 110, height: 120 }}
+					radius={100}
+				/>
+			</View>
+		);
+	}
 }
